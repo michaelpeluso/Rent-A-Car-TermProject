52037dd281c5c1b014a4b1157281431e0f314ddc
Michael Peluso <mp272@njit.edu>
2023-12-10 18:02:32 -0500

    connect to oracle

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 const oracledb = require("oracledb");
-oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 require("dotenv").config();
 
-async function run() {
-    let connection;
+async function oracleRequest() {
+    const connection = await oracledb.getConnection({
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        connectString: process.env.DB_CONNECTION_STRING,
+    });
 
-    try {
-        // Establish a connection to the Oracle database
-        connection = await oracledb.getConnection({
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            connectString: process.env.DB_CONNECTION_STRING,
-        });
-
-        // Execute SQL queries or operations here
-        const result = await connection.execute("SELECT * FROM your_table");
-
-        console.log("Query Result:", result.rows);
-    } catch (error) {
-        console.error("Error connecting to Oracle:", error);
-    } finally {
-        // Release the Oracle connection
-        if (connection) {
-            try {
-                await connection.close();
-                console.log("Connection closed");
-            } catch (error) {
-                console.error("Error closing connection:", error);
-            }
+    connection.connect((err, result) => {
+        if (err) {
+            console.log(err);
+            return;
         }
-    }
+
+        const sql = "SELECT * FROM employees";
+
+        connection.query(sql, (err, result) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            console.log(result.rows);
+
+            connection.close((err, result) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+            });
+        });
+    });
 }
 
-// Run the script
-run();
+oracleRequest();
