34e64b01bcc81091bc03080c64ea38d702d557f8
Michael Peluso <mp272@njit.edu>
2023-12-10 20:09:15 -0500

    successfully connect to database

--- index.js
+++ index.js
@@ -2,37 +2,25 @@ import { getConnection } from "oracledb";
 import dotenv from "dotenv";
 dotenv.config();
 
-async function oracleRequest() {
-    const connection = await getConnection({
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        connectString: process.env.DB_CONNECTION_STRING,
-    });
-
-    connection.connect((err, result) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-
-        const sql = "SELECT * FROM employees";
-
-        connection.query(sql, (err, result) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-
-            console.log(result.rows);
-
-            connection.close((err, result) => {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-            });
+async function connectToOracle() {
+    try {
+        // connect to Oracle Database
+        const connection = await getConnection({
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            connectionString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=prophet.njit.edu)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SID=course)))",
         });
-    });
+        console.log("Connected to Oracle Database");
+
+        // database operations
+
+        // Release connection
+        await connection.close();
+        console.log("Connection closed");
+    } catch (error) {
+        console.error("Error connecting to Oracle:", error.message);
+    }
 }
 
-oracleRequest();
+// Run the connection function
+connectToOracle();
